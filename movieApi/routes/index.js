dda6159abe8f061d2c78a8cf986c44a26a8d2b27
subodhss23@gmail.com <subodhss23@gmail.com>
2019-07-21 14:34:33 -0700

    adding most_popular

--- movieApi/routes/index.js
+++ movieApi/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const movies = require('../data/movies');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/most_popular', function(req, res, next){
+  if(req.query.api_key != 123456789){
+    res.json("Invalid API key");
+  } else {
+    const results = movies.filter(function(movie){
+      return movie.most_popular;
+  })
+  res.json(results);
+}
+});
+
+
 
 module.exports = router;
